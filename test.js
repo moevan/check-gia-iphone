c04621d3ee2af5d2a2216736e4ca67ca7cb2ede1
thimoe <thimoe>
2020-09-22 14:57:11 +0700

    done except fptshop

--- test.js
+++ test.js
@@ -20,104 +20,146 @@ const {
 const { isTokenValid, getNewToken } = require("./auth");
 const moment = require("moment"); // require
 
-
-function getDomain (link){
+function getDomain(link) {
   let regex = /https:\/\/(.*?)\//g;
-  let  res = regex.exec(link);
-  // link.match(regex)  
+  let res = regex.exec(link);
+  // link.match(regex)
   return res[1];
 }
 
-let links = [
-  {
-    site: "hoangha",
-    links: [
-        `https://hoanghamobile.com/iphone-12-series-c2651.html`,
-        `https://hoanghamobile.com/iphone-11-series-c2535.html`,
-        `https://hoanghamobile.com/iphone-88-plus-c2364.html`,
-        `https://hoanghamobile.com/iphone-se-c2154.html`,
-        `https://hoanghamobile.com/iphone-77-plus-c2363.html`,
-        `https://hoanghamobile.com/ipad-97-2018-c2325.html`,
-        `https://hoanghamobile.com/ipad-pro-c2126.html`,
-        `https://hoanghamobile.com/airpods-2-c2644.html`,
-        `https://hoanghamobile.com/airpods-pro-c2645.html`,
-        `https://hoanghamobile.com/macbook-air-c2646.html`,
-        `https://hoanghamobile.com/macbook-pro-c2647.html`,
-        `https://hoanghamobile.com/apple-watch-series-3-c2649.html`,
-        `https://hoanghamobile.com/apple-watch-series-5-c2650.html`,
-    ],
-  },
-];
-// hoang ha 
+let listWeb = {
+  hoanghamobile: [
+    `https://hoanghamobile.com/iphone-12-series-c2651.html`,
+    `https://hoanghamobile.com/iphone-11-series-c2535.html`,
+    `https://hoanghamobile.com/iphone-88-plus-c2364.html`,
+    `https://hoanghamobile.com/iphone-se-c2154.html`,
+    `https://hoanghamobile.com/iphone-77-plus-c2363.html`,
+    `https://hoanghamobile.com/ipad-97-2018-c2325.html`,
+    `https://hoanghamobile.com/ipad-pro-c2126.html`,
+    `https://hoanghamobile.com/airpods-2-c2644.html`,
+    `https://hoanghamobile.com/airpods-pro-c2645.html`,
+    `https://hoanghamobile.com/macbook-air-c2646.html`,
+    `https://hoanghamobile.com/macbook-pro-c2647.html`,
+    `https://hoanghamobile.com/apple-watch-series-3-c2649.html`,
+    `https://hoanghamobile.com/apple-watch-series-5-c2650.html`,
+  ],
+  thegioididong: [
+    `https://www.thegioididong.com/dtdd-apple-iphone`,
+    `https://www.thegioididong.com/laptop-apple-macbook`,
+    `https://www.thegioididong.com/may-tinh-bang-apple-ipad`,
+    `https://www.thegioididong.com/dong-ho-thong-minh-apple`,
+  ],
 
-async function getProducts({links}) {
-  
-  await connectToMongoDB();
-  for (link of links){
-    var domain = getDomain(link);
-    let res = await fetch(
-      link
-     );
-   
-     let text = await res.text();
+  techone: [
+    `https://www.techone.vn/iphone-11-pro-max`,
+    `https://www.techone.vn/iphone-11-pro`,
+    `https://www.techone.vn/iphone-11-cu`,
+    `https://www.techone.vn/iphone-xs-max`,
+    `https://www.techone.vn/iphone-xs`,
+    `https://www.techone.vn/iphone-xr`,
+    `https://www.techone.vn/iphone-x`,
+    `https://www.techone.vn/iphone-8-plus`,
+    `https://www.techone.vn/iphone-8`,
+    `https://www.techone.vn/iphone-7-plus`,
+    `https://www.techone.vn/iphone-7`,
+    `https://www.techone.vn/iphone-cpo`,
+    `https://www.techone.vn/macbook`,
+    `https://www.techone.vn/ipad`,
+    `https://www.techone.vn/apple-watch`,
+  ],
+  cellphones: [
+    `https://cellphones.com.vn/mobile/apple.html`,
+    `https://cellphones.com.vn/tablet/ipad-pro.html`,
+    `https://cellphones.com.vn/tablet/ipad-air.html`,
+    `https://cellphones.com.vn/tablet/ipad-mini.html`,
+    `https://cellphones.com.vn/tablet/ipad-10-2.html`,
+    `https://cellphones.com.vn/tablet/ipad-9-7.html`,
+    `https://cellphones.com.vn/do-choi-cong-nghe/apple-watch.html`,
+    `https://cellphones.com.vn/laptop/mac.html`,
+  ],
+  fptshop: [
+    `https://fptshop.com.vn/dien-thoai/apple-iphone`,
+    `https://fptshop.com.vn/may-tinh-xach-tay/apple-macbook`,
+    `https://fptshop.com.vn/may-tinh-bang/apple-ipad`,
+    `https://fptshop.com.vn/smartwatch/apple-watch`,
+  ],
+  mobilecity: [
+    `https://mobilecity.vn/dien-thoai-apple`,
+    `https://mobilecity.vn/may-tinh-bang-ipad`,
+  ],
+};
+
+// hoang ha
+async function getProducts() {
+  let $;
+  let selector = {
+    hoanghamobile: function (item) {
+      return {
+        listItem: $(".list-item"),
+        name: $("h4", ".product-name", item),
+        price: $(".product-price", item),
+      };
+    },
+    thegioididong: function (item) {
+      return {
+        listItem: $(".item"),
+        name: $("h3", item),
+        price: $("strong", ".price", item),
+      };
+    },
+    techone: function (item) {
+      return {
+        listItem: $(".product_item"),
+        name: $("a", ".product_item_title", item),
+        price: $(".amount", ".product_item_price", item),
+      };
+    },
+    cellphones: function (item) {
+      return {
+        listItem: $(".cate-pro-short"),
+        name: $("#product_link", item),
+        price: $(".price", item),
+      };
+    },
+    fptshop: function (item) {
+      return {
+        listItem: $(".cdt-product"),
+        name: $(".cdt-product__name", item),
+        price: $(".progress", item),
+      };
+    },
+    mobilecity: function (item) {
+      return {
+        listItem: $(".product-list-item"),
+        name: $(".name", item),
+        price: $(".price", item),
+      };
+    },
+
+  };
+
+  // await connectToMongoDB();
+  for (web in listWeb) {
+    for (link of listWeb[web]) {
+     
+      let res = await fetch(link);
     
-     let $ = cheerio.load(text);
+      let text = await res.text();
+      $ = cheerio.load(text);
    
-     let items = $(".list-item");
-
-     items.each((index, item) => {
-       let name = $("h4", ".product-name", item).text();
-       let price = $(".product-price", item).text();
-   
-       insertNewObject(index, { name, price, domain });
-     });
-  }
-  
-}
-
-function query(){
-  // mobile city
-  let items = $(".product-list-item");
-  items.each ( (i, phone)=>{
-    let name = $("a",".name");
-  })
-}
-const queries = {
-  
-  mobilecity: {
-    listItem: '.product-list-item',
-    name: 'a,.name',
-    price: '.price'
-  }
-}
-
-console.log(queries.mobilecity.name.split(","));
-async function getProducts({links}) {
- 
-let {listItem,name,price}=queries.mobilecity;
-  await connectToMongoDB();
-  for (link of links){
-    var domain = getDomain(link);
-    let res = await fetch(
-      link
-     );
-   
-     let text = await res.text();
+      let items = selector[web]().listItem;
     
-     let $ = cheerio.load(text);
-   
-     let items = $(listItem);
-
-     items.each((index, item) => {
-       let name = $("h4", ".product-name", item).text();
-       let price = $(".product-price", item).text();
-   
-       insertNewObject(index, { name, price, domain });
-     });
+     
+      
+      items.each((index, item) => {
+      
+        let name = selector[web](item).name.text();
+        let price = selector[web](item).price.text();
+        console.log(name, price, web);
+        // insertNewObject(index, { name, price, web });
+      });
+    }
   }
-  
 }
 
-
-
-// getProducts(links[0]);
+// getProducts();
